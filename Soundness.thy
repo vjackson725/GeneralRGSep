12c542563d36914c010574c4892383c9abffdc78
V. Jackson <vjjac@student.unimelb.edu.au>
2025-02-27 15:46:18 +1100

    clean up pconfig and cpconfig

--- Soundness.thy
+++ Soundness.thy
@@ -5,9 +5,9 @@ begin
 
 section \<open> Operational Semantics \<close>
 
-type_synonym ('s, 'a) pconfig = \<open>'s \<times> 's comm\<close>
+type_synonym 's pconfig = \<open>'s \<times> 's comm\<close>
 
-type_synonym ('s, 'a) cpconfig = \<open>('s + unit) \<times> 's comm\<close>
+type_synonym 's cpconfig = \<open>('s + unit) \<times> 's comm\<close>
 
 subsection \<open> Actions \<close>
 
@@ -20,7 +20,7 @@ lemma act_not_eq_iff[simp]:
 
 subsection \<open> Operational semantics steps \<close>
 
-fun opstep :: \<open>unit act \<Rightarrow> ('s, unit) pconfig \<Rightarrow> ('s, unit) cpconfig \<Rightarrow> bool\<close> where
+fun opstep :: \<open>unit act \<Rightarrow> 's pconfig \<Rightarrow> 's cpconfig \<Rightarrow> bool\<close> where
   \<open>opstep \<alpha> (h, Skip) s' \<longleftrightarrow> False\<close>
 | \<open>opstep \<alpha> (h, c1 ;; c2) s' \<longleftrightarrow>
     \<alpha> = Tau \<and> c1 = Skip \<and> s' = (Inl h, c2) \<or>
