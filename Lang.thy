c7ce87d338fb170498ccc193a62c13d4179d155b
V. Jackson <vjackson725@users.noreply.github.com>
2026-01-01 19:20:40 +1100

    TMP: fix wssa/sswa bot rel proofs

--- Lang.thy
+++ Lang.thy
@@ -521,12 +521,10 @@ lemmas wssa_stronger_strengthen =
 lemma sswa_bot_rel_eq[simp]:
   \<open>sswa \<bottom> p = p\<close>
   by (clarsimp simp add: sp_def fun_eq_iff)
-    (metis (full_types) rtranclp_eq_eq rtranclp_reflclp sup_bot_left)
 
 lemma wssa_bot_rel_eq[simp]:
   \<open>wssa \<bottom> p = p\<close>
   by (clarsimp simp add: wlp_def fun_eq_iff)
-    (metis (full_types) rtranclp_eq_eq rtranclp_reflclp sup_bot_left)
 
 
 subsection \<open> absorption/pseduo-idempotence properties \<close>
